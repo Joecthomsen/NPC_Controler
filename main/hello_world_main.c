dca009a5c64c200789dacce9faaa6854d79db7a1
joecthomsen <joecthomsen@gmail.com>
2024-02-14 15:56:01 +0100

    Includes completed

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -19,6 +19,13 @@
 #include "driver/gptimer.h"
 #include "esp_log.h"
 
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <nvs_flash.h>
+#include <wifi_provisioning/manager.h>
+#include <wifi_provisioning/scheme_softap.h>
+#include "qrcode.h"
+
 void taskOne(void *parameter)
 {
     while (true)
