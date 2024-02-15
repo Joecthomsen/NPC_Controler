79baf56b378ac012d48274fd000bec672eb29169
joecthomsen <joecthomsen@gmail.com>
2024-02-15 14:38:48 +0100

    Added prototypes in wifi_provisioning.c

--- components/Wifi_provisioning/Wifi_provisioning.c
+++ components/Wifi_provisioning/Wifi_provisioning.c
@@ -9,6 +9,12 @@
 #include "esp_log.h"
 #include <string.h>
 
+// Prototypes
+static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+static void get_device_service_name(char *service_name, size_t max);
+esp_err_t custom_prov_data_handler(uint32_t session_id, const uint8_t *inbuf, ssize_t inlen, uint8_t **outbuf, ssize_t *outlen, void *priv_data);
+static void wifi_prov_print_qr(const char *name, const char *username, const char *pop, const char *transport);
+
 static const char *TAG = "app";
 
 /* Signal Wi-Fi events on this event-group */
